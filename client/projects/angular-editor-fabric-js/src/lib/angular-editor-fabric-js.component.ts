157ba7e689e6d3a91e4a79f225ab3945973b68f6
Anurag Pradhan <apradhan@argusoft.com>
2023-05-12 16:10:58 +0530

    Updated Template

--- client/projects/angular-editor-fabric-js/src/lib/angular-editor-fabric-js.component.ts
+++ client/projects/angular-editor-fabric-js/src/lib/angular-editor-fabric-js.component.ts
@@ -33,7 +33,7 @@ export class FabricjsEditorComponent implements AfterViewInit {
   public textString: string;
   public url: string | ArrayBuffer = '';
   public size: any = {
-    width: 684,
+    width: 600,
     height: 1010
   };
 
